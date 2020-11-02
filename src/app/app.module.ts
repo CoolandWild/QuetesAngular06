1a85f15178d6332d7f7f0ea209406633597ff5be
CoolandWild <eric.boileaux@sncf.fr>
2020-11-02 10:42:28 +0100

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { User} from './User';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+  model: User = new User();
+
+  onSubmit() {
+    // form submitted
+    console.log(this.model);
+    }
+  ngOnInit(): void {
+
+  }
+
+}
